d51aad0fd20667ed36d43117cc321de0a3ab22be
nhh226041 <Hoang.NH226041@sis.hust.edu.vn>
2023-06-15 22:10:04 +0700

    accept

--- .vscode/KefaAndPark.cpp
+++ .vscode/KefaAndPark.cpp
@@ -1,125 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m;
-int a[100000];
-int count = 0;
-struct Node{
-    int data;
-    int cat;
-    Node* leftmost_child;
-    Node* right_sibling;
-};
-typedef struct Node* node;
-node makeNode(int x, int cat){
-    node newNode = new Node;
-    if(newNode!=NULL){
-        newNode->data = x;
-        newNode->cat = cat;
-        newNode->leftmost_child = NULL;
-        newNode->right_sibling = NULL;
-    }
-    return newNode;
-}
-
-node find(node r, int u){
-    if(r==NULL) return NULL;
-    if(r->data == u) return r;
-    else{
-        node p = r->leftmost_child;
-        while(p!=NULL){
-            node result = find(p, u);
-            if(result!=NULL) return result;
-            p=p->right_sibling;
-        }
-    }
-    return NULL;
-}
-    int num=0;
-void insert(node &r, int u, int v, int cat){
-    if(find(r, u)==NULL || find(r, v)!=NULL){
-        // printf("* ");
-        num=1;
-        return;
-    }
-    if(r==NULL){
-        r = makeNode(v, cat);
-    }else{
-        node parent = find(r, u);
-        if(parent->leftmost_child==NULL){
-            parent->leftmost_child = makeNode(v, cat);
-        }else{
-            node p = parent->leftmost_child;
-            while(p->right_sibling!=NULL){
-                p=p->right_sibling;
-            }
-            p->right_sibling = makeNode(v, cat);
-        }
+vector <int> a[100010];
+int n, m, total = 0;
+int check[100010] = {0};
+int cat[100010];
+void solve(int s, int sum){
+    //kiểm tra node này đã được xét chưua
+    if(check[s]) return;
+    check[s] = 1;
+    //Tính tổng số cat trên đoạn
+    if(cat[s]) sum++;
+    else sum = 0;
+    //kết thúc đệ quy khi sum>m
+    if(sum>m) return;
+    //Kiểm tra lá này có hợp lệ không
+    if(s!=1 && a[s].size()==1) total++;
+    //Đệ quy những theo DFS
+    for(int i = 0; i < a[s].size(); i++){
+        solve(a[s][i], sum);
     }
 }
-
-void PreOrder(node r){
-    if(r==NULL) return;
-    cout<<r->data<<" ";
-    cout<<r->cat<<"\n";
-    node p = r->leftmost_child;
-    while(p!=NULL){
-        PreOrder(p);
-        p=p->right_sibling;
-    }
-}
-void solve(node r, int pathCat){
-    if(r==NULL || pathCat>m){
-        return;
-    }
-    // if(r->cat==0 && r->leftmost_child!=NULL){
-    //     solve(r->leftmost_child, 0);
-    // }
-    if(r->leftmost_child==NULL){
-            if(pathCat<=m){
-                // cout<<r->data<<" ";
-                count++;
-            }
-    }
-    // cout<<r->data<<" ";
-    node p =r->leftmost_child;
-    while(p!=NULL){
-        if(p->cat==0) solve(p, 0);
-        else solve(p, pathCat+p->cat);
-        p=p->right_sibling;
-    }
-}
-
 int main(){
-    node root = NULL;
+    int u, v;
     cin>>n>>m;
     for(int i = 1; i <= n; i++){
-        cin>>a[i];
+        cin>>cat[i];
     }
-    root = makeNode(1, a[1]);
-    // insert(root,1,1,a[1]);
-    int x, y;
     for(int i = 2; i <= n; i++){
-        cin>>x>>y;
-        insert(root, x, y, a[i]);
+        cin>>u>>v;
+        a[u].push_back(v);
+        a[v].push_back(u);
     }
-    // cout<<"\n";
-    // root = makeNode(1, 0);
-    // root->leftmost_child = makeNode(2, 0);
-    // root->leftmost_child->right_sibling = makeNode(3, 0);
-    // root->leftmost_child->right_sibling->leftmost_child=makeNode(4, 0);
-    // root->leftmost_child->right_sibling->leftmost_child->right_sibling=makeNode(5, 0);
-    //     cout<<"\n"<<num<<"\n";
-    // PreOrder(root);
-    cout<<"\n";
-    if(num){
-        cout<<"*";
-        cout<<count;
-    }else{
-        solve(root, a[1]);
-        cout<<count;
-    }
-
-    // if(find(root, 3)!=NULL){
-    //     cout<<"Yes";
-    // }else cout<<"No";
+    solve(1, 0);
+    cout<<total;
 }
